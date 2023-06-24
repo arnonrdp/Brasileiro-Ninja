43c2b9309198c3ffd5cafa8e6077da41c2b1c303
Arnon <arnonrdp@gmail.com>
2023-06-24 18:03:59 -0300

    Finished home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>This is the Home Page</h1>
+  <div class="description">
+    <h1>Este é o Brasileiro Ninja</h1>
+    <h3>
+      O objetivo deste projeto é oferecer consultas confiáveis aos principais serviços do Brasil, de
+      forma totalmente gratuita. Para isso, utilizamos o Brasil API como ponto de acesso. O Brasil
+      API tem como propósito centralizar e disponibilizar endpoints modernos com uma latência
+      extremamente baixa, independentemente da fonte dos dados.
+    </h3>
+    <h3>
+      Deseja colaborar com este projeto? Suas contribuições são muito bem-vindas e podem ser feitas
+      por meio do GitHub, onde você pode compartilhar suas ideias, sugerir melhorias e enviar
+      feedback. Sua participação é essencial para tornar este projeto ainda melhor. Junte-se a nós e
+      faça a diferença!
+    </h3>
+    <a href="https://github.com/arnonrdp/brasileiro-ninja">
+      <img alt="Logo Brasil API" src="src/assets/github.svg" />
+    </a>
+    <a href="https://brasilapi.com.br/" target="_blank">
+      <img alt="Logo Brasil API" src="src/assets/brasilapi.webp" />
+    </a>
+  </div>
 </template>
+
+<style scoped>
+.description {
+  text-align: center;
+}
+
+h1,
+h3 {
+  margin: 5rem 2rem;
+}
+
+img {
+  height: 4rem;
+  margin: 0 1rem;
+}
+</style>
