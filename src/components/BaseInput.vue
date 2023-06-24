2dbcd977118d020980b34e0ff2548d0168cb61ea
Arnon <arnonrdp@gmail.com>
2023-06-24 18:31:10 -0300

    Adicionado BaseInput para ser reutilizado

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input placeholder="Pesquisar" v-model="value" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<style scoped>
+input {
+  display: block;
+  font-size: 1.5rem;
+  margin: 2rem auto;
+  max-width: 90vw;
+  width: 20rem;
+}
+</style>
