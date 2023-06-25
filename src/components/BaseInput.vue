ae15ed523deb8c4c41f684c15e83a5163a891fdc
Arnon <arnonrdp@gmail.com>
2023-06-25 20:51:12 -0300

    Improved `BaseInput`with button

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,12 +1,15 @@
 <template>
-  <input autofocus placeholder="Pesquisar" v-model="value" />
+  <form @submit.prevent="search">
+    <input autofocus placeholder="Pesquisar" v-model="value" />
+    <button type="submit">&#128269;</button>
+  </form>
 </template>
 
 <script setup>
 import { computed } from 'vue'
 
 const props = defineProps(['modelValue'])
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue', 'search'])
 
 const value = computed({
   get() {
@@ -16,20 +19,29 @@ const value = computed({
     emit('update:modelValue', value)
   }
 })
+
+const search = () => {
+  emit('search')
+}
 </script>
 
 <style scoped>
+form {
+  margin: 2rem auto;
+  overflow: hidden;
+  position: relative;
+  width: 30rem;
+}
 input {
   border: 1px solid #ccc;
   border-radius: 24px;
   display: block;
   font-size: 1.2rem;
   height: 2.5rem;
-  margin: 2rem auto;
   max-width: 90vw;
   text-align: center;
   transition: all 0.2s ease-in-out;
-  width: 25rem;
+  width: 100%;
 }
 
 input:focus {
@@ -40,5 +52,18 @@ input:focus {
 
 input::placeholder {
   opacity: 0.5;
+}
+
+button {
+  background: hsla(160, 100%, 37%, 0.2);
+  border-radius: 24px;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2rem;
+  height: 2.5rem;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 4.5rem;
 }
 </style>
