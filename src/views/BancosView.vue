8733eb267fb654f0fb4892feedabf75ec8fac16b
Arnon <arnonrdp@gmail.com>
2023-06-25 11:08:22 -0300

    Added basic table for banks

--- src/views/BancosView.vue
+++ src/views/BancosView.vue
@@ -2,11 +2,37 @@
   <h1>Bancos</h1>
   <h2>Informações de todos os bancos do Brasil</h2>
   <BaseInput v-model="search" />
+  <table>
+    <thead>
+      <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Razão Social</th>
+        <th>ISPB</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="bank in banks" :key="bank.code">
+        <td>{{ bank.code }}</td>
+        <td>{{ bank.name }}</td>
+        <td>{{ bank.fullName }}</td>
+        <td>{{ bank.ispb }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import BaseInput from '@/components/BaseInput.vue'
-import { ref } from 'vue'
+import { readBanks } from '@/stores/bancos'
+import { onMounted, ref } from 'vue'
 
+const banks = ref([])
 const search = ref('')
+
+onMounted(async () => {
+  await readBanks().then((response) => {
+    banks.value = response
+  })
+})
 </script>
